82ca0086b160633e13823a8466842d88b891a88b
otterpro <coconutstudio@yahoo.com>
2015-03-06 09:50:31 -0800

    nv.sh correct path in linux

--- nv.sh
+++ nv.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 # Daniel Kim (otter.pro): load vim after cd into dropbox/notes
-cd ~/Dropbox/_notes
-mvim +CtrlP
+case "$OSTYPE" in
+	darwin*) 
+		note_path=~/Dropbox/_notes
+		editor=mvim
+		;;
+	*) 
+		note_path=~/Dropbox/_notes
+		editor=vim
+		;;
+esac
+	cd $note_path
+	$editor +CtrlP
