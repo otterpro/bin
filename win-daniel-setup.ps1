f9a6ea89bdb430393a132cfe32e15e3f646ce944
otter.pro <codequickly@yahoo.com>
2017-05-30 11:06:16 -0400

    win-daniel-setup.ps1 - add keyboard - swap ctrl with capslock

--- win-daniel-setup.ps1
+++ win-daniel-setup.ps1
@@ -1,2 +1,36 @@
+Write-Host "windows setup"
 # MYVIMRC="C:\cygwin64\home\Dkim"
-[Environment]::SetEnvironmentVariable("MYVIMRC", "C:\Cygwin64\home\$env:username", "User")
+# [Environment]::SetEnvironmentVariable("MYVIMRC", "C:\Cygwin64\home\$env:username", "User")
+
+#===========================================================================
+# Explorer: show Hidden files and show all extensions
+#===========================================================================
+# same as : Set Explorer Windows option 
+    # Choose "Show hidden files, folders, and drives"
+    # Uncheck "Hide extensions for known file types"
+    # Uncheck "Hide protected operating system files (Recommended)"
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+if ((Get-ItemProperty $key).HideFileExt -ne 0) {
+    # Do this only if necessary!  Get-ItemProperty first
+    Write-Host "Setting Explorer to show hidden files and extensions"
+    Set-ItemProperty $key Hidden 1
+    Set-ItemProperty $key HideFileExt 0
+    Set-ItemProperty $key ShowSuperHidden 1
+    Stop-Process -processname explorer
+}
+
+#===========================================================================
+# Swap Ctrl with CapsLock and assign Left ALT to Right CTRL
+#===========================================================================
+# see ~/bin/winkb*.reg
+# Note: The user must re-login? /reboot to take effect
+# setting binValues in registry: https://stackoverflow.com/a/33586470
+#Change these three to match up to the extracted registry data and run as Admin
+$key = 'HKLM:\SYSTEM\CurrentControlSet\Control\Keyboard Layout'
+$attribute = "Scancode Map"
+$propValue = (Get-ItemProperty $key).$attribute 
+if (!$propValue) {  #if reg value doesn't exist
+    $binValues = "00,00,00,00,00,00,00,00,03,00,00,00,1d,00,3a,00,1d,e0,38,00,00,00,00,00"
+    $hexified = $binValues.Split(',') | % { "0x$_"}
+    New-ItemProperty -Path $key -Name $attribute -PropertyType Binary -Value ([byte[]]$hexified)
+}
