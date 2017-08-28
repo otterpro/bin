04487ed4d7250f8e7b74dbe6f4b7cf59c93b2c00
otter.pro <coconutstudio@yahoo.com>
2017-08-28 14:20:06 -0400

    mac-setup.sh created

---
+++ mac-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# setup script for Mac
+
+# xcode cmdline tool
+xcode-select --install
+
+# homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+brew install coreutils
+brew install binutils
+brew install diffutils
+brew install gzip
+brew install watch
+brew install tmux
+brew install wget
+brew install nmap
+brew install gpg
+brew install htop
+brew install trash
+brew install m-cli
+brew install macvim
+brew install vlc
+
+# wishlist
+# beardspice
+# zerotier and setup
+# amazon music
+# chrome?
+# firefox?
+# evernote
+
+# ffmpeg
+# brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools
+
