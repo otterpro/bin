7cdb1018f2112a87aeead1d99319b5ec0a26c111
otter.pro <coconutstudio@yahoo.com>
2017-10-25 10:24:57 -0400

    changed path on win

--- push_notes.bat
+++ push_notes.bat
@@ -1,11 +1,11 @@
-REM Put in Scheduled Task to update _notes daily
-set PATH=c:\cygwin64\bin;%PATH%
-set NOTES_PATH=c:\cygwin64\home\dkim\Dropbox\_notes
+REM Put in Scheduled Task to update _notes daily
+set PATH=c:\cygwin64\bin;%PATH%
+set NOTES_PATH=c:\cygwin64\home\dkim\notes
 
 
-cd %NOTES_PATH%
-git add -A
-git commit -m"scheduled update"
-git pull --no-edit
-git push
-exit 0
+cd %NOTES_PATH%
+git add -A
+git commit -m"scheduled update"
+git pull --no-edit
+git push
+exit 0
