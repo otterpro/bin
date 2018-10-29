069be672fe7928cbf7ddcd4695e4673a2f082644
otter.pro <coconutstudio@yahoo.com>
2018-10-28 20:22:19 -0400

    added date message

--- win/backup_hd0.sh
+++ win/backup_hd0.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+echo "======================"
+echo "HD0(TM0.sparsebundle): "
 date "+%Y-%m-%d:%H:%M:%S"
 if [ -d /cygdrive/o/TM0.sparsebundle ] && [ -d /cygdrive/p ] ; then
-	rsync -av --del -v /cygdrive/o/TM0.sparsebundle/ /cygdrive/p/TM0.sparsebundle
+	rsync -av --del /cygdrive/o/TM0.sparsebundle/ /cygdrive/p/TM0.sparsebundle
 fi
