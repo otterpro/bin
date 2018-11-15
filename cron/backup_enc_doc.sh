78abc9eacc9386ee3155d537a2a720350f5f4ee9
otter.pro <coconutstudio@yahoo.com>
2018-11-14 19:45:17 -0500

    copied old script files

--- cron/backup_enc_doc.sh
+++ cron/backup_enc_doc.sh
@@ -3,6 +3,6 @@
 echo "======================"
 echo "doc.sparsebundle: "
 date "+%Y-%m-%d:%H:%M:%S"
-if [ -d /Volumes/?????/doc.sparsebundle ] && [ -d /Users/otter/Box\ Sync ] ; then
-	rsync -av --del /Volumes/?????/doc.sparsebundle/ /Users/otter/Box\ Sync/backups/doc.sparsebundle
+if [ -d /Volumes/HD0/doc.sparsebundle ] && [ -d /Users/otter/Box\ Sync ] ; then
+	rsync -av --del /Volumes/HD0/doc.sparsebundle/ /Users/otter/Box\ Sync/backups/doc.sparsebundle
 fi
