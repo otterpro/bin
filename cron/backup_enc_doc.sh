4e6808c219843245dab5b2ea414c748add347d64
otter.pro <coconutstudio@yahoo.com>
2018-11-17 16:16:52 -0500

    install_vscode.sh: change dir to _VSCode

--- cron/backup_enc_doc.sh
+++ cron/backup_enc_doc.sh
@@ -4,5 +4,5 @@ echo "======================"
 echo "doc.sparsebundle: "
 date "+%Y-%m-%d:%H:%M:%S"
 if [ -d /Volumes/HD0/doc.sparsebundle ] && [ -d /Users/otter/Box\ Sync ] ; then
-	rsync -av --del /Volumes/HD0/doc.sparsebundle/ /Users/otter/Box\ Sync/backups/doc.sparsebundle
+	/usr/bin/rsync -av --del /Volumes/HD0/doc.sparsebundle/ /Users/otter/Box\ Sync/backups/doc.sparsebundle
 fi
